8165a258f62606cd536cd345ea91fa3a0e86070f
RedLime <redlimerl@gmail.com>
2023-07-09 18:29:42 +0900

    Update accurate sha helper

--- ModFileLoader/src/main/java/com/redlimerl/mcsr/helper/ShaHelper.java
+++ ModFileLoader/src/main/java/com/redlimerl/mcsr/helper/ShaHelper.java
@@ -1,50 +1,17 @@
 package com.redlimerl.mcsr.helper;
 
+import org.apache.commons.codec.digest.DigestUtils;
+
 import java.io.IOException;
 import java.io.InputStream;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 public class ShaHelper {
-    public static String getSha1FromInputStream(InputStream inputStream) throws NoSuchAlgorithmException, IOException {
-        MessageDigest digest = MessageDigest.getInstance("SHA-1");
-        int n = 0;
-        byte[] buffer = new byte[8192];
-        while (n != -1) {
-            n = inputStream.read(buffer);
-            if (n > 0) {
-                digest.update(buffer, 0, n);
-            }
-        }
 
-        byte[] hash = digest.digest();
-
-        StringBuilder sb = new StringBuilder(hash.length * 2);
-        for (byte b : hash) {
-            sb.append(Integer.toString((b & 0xf0) >> 4, 16));
-            sb.append(Integer.toString(b & 0x0f, 16));
-        }
-        return sb.toString();
+    public static String getSha1FromInputStream(InputStream inputStream) throws IOException {
+        return DigestUtils.sha1Hex(inputStream);
     }
 
-    public static String getSha512FromInputStream(InputStream inputStream) throws NoSuchAlgorithmException, IOException {
-        MessageDigest digest = MessageDigest.getInstance("SHA-512");
-        int n = 0;
-        byte[] buffer = new byte[8192];
-        while (n != -1) {
-            n = inputStream.read(buffer);
-            if (n > 0) {
-                digest.update(buffer, 0, n);
-            }
-        }
-
-        byte[] hash = digest.digest();
-
-        StringBuilder sb = new StringBuilder(hash.length * 2);
-        for (byte b : hash) {
-            sb.append(Integer.toString((b & 0xf0) >> 4, 16));
-            sb.append(Integer.toString(b & 0x0f, 16));
-        }
-        return sb.toString();
+    public static String getSha512FromInputStream(InputStream inputStream) throws IOException {
+        return DigestUtils.sha512Hex(inputStream);
     }
 }
