79488d60c8b33255d93610c86e6e6a44c5bbfeb4
RedLime <redlimerl@gmail.com>
2023-07-09 20:49:58 +0900

    modpack generator

--- ModFileLoader/src/main/java/com/redlimerl/mcsr/MCSRModLoader.java
+++ ModFileLoader/src/main/java/com/redlimerl/mcsr/MCSRModLoader.java
@@ -1,6 +1,8 @@
 package com.redlimerl.mcsr;
 
 import com.google.gson.*;
+import com.redlimerl.mcsr.helper.HttpRequestHelper;
+import com.redlimerl.mcsr.helper.MRPackHelper;
 import com.redlimerl.mcsr.mod.FabricMod;
 import com.redlimerl.mcsr.mod.Loader;
 import com.redlimerl.mcsr.mod.Optifine;
@@ -10,11 +12,14 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.*;
 
 public class MCSRModLoader {
 
-    private static final Path MODS_JSON_PATH = Paths.get("./meta/v4/mods.json");
-    private static final Path RESULT_JSON_PATH = Paths.get("./meta/v4/files.json");
+    public static final String VERSION = "v4";
+    private static final Path MODS_JSON_PATH = Paths.get("./meta/" + VERSION + "/mods.json");
+    private static final Path RESULT_JSON_PATH = Paths.get("./meta/" + VERSION + "/files.json");
+    private static final Path MODPACKS_PATH = Paths.get("./modpacks/" + VERSION);
     public static final Gson GSON = new GsonBuilder().serializeNulls().disableHtmlEscaping().create();
     public static String GITHUB_TOKEN = "none";
 
@@ -22,6 +27,8 @@ public class MCSRModLoader {
         GITHUB_TOKEN = args[0];
         JsonArray modList = JsonParser.parseString(Files.readString(MODS_JSON_PATH)).getAsJsonArray();
         JsonArray jsonArray = new JsonArray();
+        List<ModInfo> modInfoList = new ArrayList<>();
+        ModInfo fabricLoader = null;
 
         for (JsonElement jsonElement : modList) {
             String type = jsonElement.getAsJsonObject().get("type").getAsString();
@@ -35,10 +42,65 @@ public class MCSRModLoader {
             if (modInfo == null) continue;
 
             System.out.printf("Start downloading '%s'...%n", modInfo.getName());
+            modInfoList.add(modInfo);
             jsonArray.add(modInfo.toJson());
             System.out.printf("Done with download '%s'%n", modInfo.getName());
+
+            if (Objects.equals(modInfo.getName(), "Fabric Loader")) fabricLoader = modInfo;
         }
 
         Files.writeString(RESULT_JSON_PATH, GSON.toJson(jsonArray), StandardCharsets.UTF_8);
+
+        if (fabricLoader == null) return;
+
+        /*
+          For generate .mrpack files
+         */
+
+        Set<String> osSet = Set.of("Windows", "OSX", "Linux");
+        Set<String> typeSet = Set.of("RSG", "SSG");
+        Set<String> versionSet = Set.of("1.14.4", "1.15.2", "1.16.1", "1.16.5", "1.17.1");
+
+        for (String os : osSet) {
+            for (String type : typeSet) {
+                for (String version : versionSet) {
+                    Map<String, String> map = new HashMap<>();
+                    map.put("os", os.toLowerCase(Locale.ROOT));
+                    map.put("type", type.toLowerCase(Locale.ROOT));
+                    JsonObject mrPack = MRPackHelper.convertPack("MCSR " + type, version, fabricLoader, modInfoList, map);
+                    Path path = MODPACKS_PATH.resolve(String.format("MCSR-%s-%s-%s.mrpack", version, os, type));
+
+                    MRPackHelper.writeZipFile(path, GSON.toJson(mrPack));
+                }
+            }
+        }
+
+
+        /*
+          For generate MCSR Ranked .mrpack files
+         */
+
+        JsonObject rankedModrinth = HttpRequestHelper.getJsonFromUrl("https://api.modrinth.com/v2/project/mcsr-ranked/version?featured=true")
+                .getAsJsonArray().get(0).getAsJsonObject()
+                .getAsJsonArray("files").get(0).getAsJsonObject();
+        JsonObject ranked = MRPackHelper.getFileObject(
+                rankedModrinth.get("filename").getAsString(),
+                rankedModrinth.getAsJsonObject("hashes").get("sha1").getAsString(),
+                rankedModrinth.getAsJsonObject("hashes").get("sha512").getAsString(),
+                rankedModrinth.get("url").getAsString(),
+                rankedModrinth.get("size").getAsInt()
+        );
+        for (String os : osSet) {
+            Map<String, String> map = new HashMap<>();
+            map.put("os", os.toLowerCase(Locale.ROOT));
+            map.put("type", "rsg");
+            map.put("condition", "medical_issue");
+            JsonObject mrPack = MRPackHelper.convertPack("MCSR Ranked", "1.16.1", fabricLoader, modInfoList, map);
+
+            mrPack.getAsJsonArray("files").add(ranked);
+
+            Path path = MODPACKS_PATH.resolve(String.format("MCSRRanked-%s-1.16.1.mrpack", os));
+            MRPackHelper.writeZipFile(path, GSON.toJson(mrPack));
+        }
     }
 }
---
+++ ModFileLoader/src/main/java/com/redlimerl/mcsr/helper/MRPackHelper.java
@@ -0,0 +1,95 @@
+package com.redlimerl.mcsr.helper;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.redlimerl.mcsr.MCSRModLoader;
+import com.redlimerl.mcsr.mod.abst.ModAsset;
+import com.redlimerl.mcsr.mod.abst.ModInfo;
+import com.redlimerl.mcsr.mod.abst.ModRule;
+import net.fabricmc.loader.api.Version;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.text.SimpleDateFormat;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Map;
+import java.util.Objects;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class MRPackHelper {
+    public static JsonObject convertPack(String name, String gameVersion, ModInfo loader, Collection<ModInfo> mods, Map<String, String> rules) throws Throwable {
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("formatVersion", 1);
+        jsonObject.addProperty("game", "minecraft");
+        jsonObject.addProperty("versionId", MCSRModLoader.VERSION + "+" + new SimpleDateFormat("yy.MM.dd").format(new Date()) + "-" + gameVersion);
+        jsonObject.addProperty("name", name + " (for " + gameVersion + ")");
+
+        JsonArray jsonArray = new JsonArray();
+        for (ModInfo mod : mods) {
+            if (!Objects.equals(mod.getType(), "fabric_mod")) continue;
+            if (!mod.isRecommended()) continue;
+            assetCheck: for (ModAsset modAsset : mod.getAssetsResult()) {
+                if (!modAsset.mcVersion().test(Version.parse(gameVersion))) continue;
+                if (modAsset.sha1() == null || modAsset.sha512() == null) continue;
+                if (modAsset.rules() != null && !modAsset.rules().isEmpty()) {
+                    for (ModRule rule : modAsset.rules()) {
+                        boolean result = false;
+                        for (Map.Entry<String, String> entry : rule.properties().entrySet()) {
+                            if (!rules.containsKey(entry.getKey())) {
+                                result = false;
+                                break;
+                            } else if (!rules.get(entry.getKey()).equals(entry.getValue())) {
+                                result = false;
+                                break;
+                            } else {
+                                result = true;
+                            }
+                        }
+                        if (result != Objects.equals(rule.action(), "allow")) continue assetCheck;
+                    }
+                }
+
+                jsonArray.add(getFileObject(modAsset.fileName(), modAsset.sha1(), modAsset.sha512(), modAsset.downloadUrl(), modAsset.size()));
+                break;
+            }
+        }
+        jsonObject.add("files", jsonArray);
+
+        JsonObject dependencies = new JsonObject();
+        dependencies.addProperty("fabric-loader", loader.getAssetsResult().get(0).modVersion());
+        dependencies.addProperty("minecraft", gameVersion);
+        jsonObject.add("dependencies", dependencies);
+
+        return jsonObject;
+    }
+
+    public static JsonObject getFileObject(String fileName, String sha1, String sha512, String downloadUrl, int downloadSize) {
+        JsonObject fileData = new JsonObject();
+        fileData.addProperty("path", "mods/" + fileName);
+        JsonObject hashes = new JsonObject();
+        hashes.addProperty("sha1", sha1);
+        hashes.addProperty("sha512", sha512);
+        fileData.add("hashes", hashes);
+        JsonObject env = new JsonObject();
+        env.addProperty("client", "required");
+        env.addProperty("server", "unsupported");
+        fileData.add("env", env);
+        JsonArray downloads = new JsonArray();
+        downloads.add(downloadUrl);
+        fileData.add("downloads", downloads);
+        fileData.addProperty("fileSize", downloadSize);
+        return fileData;
+    }
+
+    public static void writeZipFile(Path path, String packData) throws IOException {
+        ZipOutputStream out = new ZipOutputStream(new FileOutputStream(path.toFile()));
+        out.putNextEntry(new ZipEntry("modrinth.index.json"));
+        out.write(packData.getBytes(StandardCharsets.UTF_8));
+        out.closeEntry();
+        out.close();
+    }
+}
